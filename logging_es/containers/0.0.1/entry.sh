824a8eeb476a974efd767d9ccc0891800feabb5a
Aiwantaozi <michelia.feng@gmail.com>
2017-12-04 11:05:45 +0800

    not unmount docker sock

--- logging_es/containers/0.0.1/entry.sh
+++ logging_es/containers/0.0.1/entry.sh
@@ -4,7 +4,7 @@ set -e
 
 for i in $(curl -s --unix /var/run/docker.sock http://localhost/info | jq -r .DockerRootDir) /var/lib/docker /run /var/run; do
     for m in $(tac /proc/mounts | awk '{print $2}' | grep ^${i}/); do
-        if [ "$m" != "/run/log/journal"  ]; then
+        if [ "$m" != "/run/log/journal" ]&&[ "$m" != "/run/docker.sock" ]; then
             umount $m || true
         fi
     done
